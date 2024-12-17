b458e697be6cc311cb7007f0d5dabe706cb145e3
somabenfell <sbenf999@outlook.com>
2024-12-17 12:15:43 +0000

    Ui additions for recording a delivery

--- src/mainApp.py
+++ src/mainApp.py
@@ -5,6 +5,7 @@ from changePassword import *
 from login_process import *
 from popUpWindow import *
 from windowSuperClass import superWindow
+from time import gmtime, strftime
 
 class App(superWindow):
 
@@ -19,6 +20,7 @@ class App(superWindow):
         # configure window
         self.title("OneStop Stock Assistant System")
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        self.resizable(False, False)
 
         # configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
@@ -63,14 +65,17 @@ class App(superWindow):
         self.sidebar_button_7 = customtkinter.CTkButton(self.sidebar_frame, command=lambda: self.goToTab("Settings"), text="Settings")
         self.sidebar_button_7.grid(row=13, column=0, padx=20, pady=(10,10))
 
-        self.setButtonStates()
-
         #tabview in which all UI will take place to do with functions of the application - the sidebar on the side simply allows for easier switching of the tabs
-        #height=(self.onResize()[0]), width=(self.onResize()[1])
+        self.setButtonStates() #set the button states (disabled or enabled) based on the user access
+        
         self.tabview = customtkinter.CTkTabview(master=self)
         self.tabview.grid(column=1, row=0)
         for tab in self.allowances[int(self.userAccessLevel)]:
             self.tabview.add(tab)
+
+        #<========================UI-SETTERS========================>
+        #STOCK TAKING TOOLS
+        self.recordDeliveryUI()
 
     #function for buttons in the sidebar - used for navigating the tabview on the right
     def goToTab(self, tabName):
@@ -82,7 +87,7 @@ class App(superWindow):
         self.buttonsDefault = [self.sidebar_button_1, self.sidebar_button_2, self.sidebar_button_3, self.sidebar_button_4, self.sidebar_button_5, self.sidebar_button_6, self.sidebar_button_7]
         self.tabs = self.tabsDefault
         self.allowances: dict = {
-                1: list(filter(lambda tab_: tab_ not in ["Profit margins"], self.tabs)),
+                1: self.tabsDefault,
                 2: list(filter(lambda tab_: tab_ not in ["Profit margins"], self.tabs)),
                 3: list(filter(lambda tab_: tab_ not in ["Profit margins", "Add product", "Add supplier", "Data view", "Weekly report", "Settings"], self.tabs))
         }
@@ -95,6 +100,29 @@ class App(superWindow):
                     if name == page:
                         button.configure(state="disabled")
 
+    def recordDeliveryUI(self, tab_='Record a delivery'): #you might want to make this a scrollable fram
+        #you need to create a supplier database and then select all suppliers in order to be able to give values for the value list below
+        self.chooseSupplierLabel = customtkinter.CTkLabel(self.tabview.tab(tab_), text="Choose supplier:", anchor="w")
+        self.chooseSupplierLabel.grid(row=0, column=0, padx=(20, 20), pady=20)
+        self.chooseSupplier = customtkinter.CTkOptionMenu(self.tabview.tab(tab_), dynamic_resizing=False, values=["Value 1", "Value 2", "Value Long Long Long"]) #values list should be taken from a database call once the supplier database is created
+        self.chooseSupplier.grid(row=0, column=1, padx=20, pady=20)
+        self.enterDeliveryDateLabel = customtkinter.CTkLabel(self.tabview.tab(tab_), text="Enter delivery date:")
+        self.enterDeliveryDateLabel.grid(row=1, column=0, padx=(20, 20), pady=20)
+
+        #have 3 boxes for this, aka, day, month, year
+        self.deliveryDateEntry = customtkinter.CTkEntry(self.tabview.tab(tab_), placeholder_text="Delivery date xx/xx/xx...")
+        self.deliveryDateEntry.grid(row=1, column=1, columnspan=2, sticky="nsew", padx=(20, 20), pady=20)
+        self.enterDeliveryTimeLabel = customtkinter.CTkLabel(self.tabview.tab(tab_), text="Delivery time:")
+        self.enterDeliveryTimeLabel.grid(row=2, column=0, padx=(20, 20), pady=20)
+
+        #delivery time shoud create an ovveride feature if user doesnt want to use current system time
+        self.deliveryTime = strftime("%X", gmtime())
+        self.enterDeliveryTimeLabelAbs = customtkinter.CTkLabel(self.tabview.tab(tab_), text=self.deliveryTime)
+        self.enterDeliveryTimeLabelAbs.grid(row=2, column=1, padx=(20, 20), pady=20)
+
+
 if __name__ == "__main__":
-    login = Logon()
-    login.mainloop()
+    #login = Logon()
+    #login.mainloop()
+    app = App(1)
+    app.mainloop()
